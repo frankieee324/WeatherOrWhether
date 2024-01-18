eb4e5d9b750697767edce748e90b8a5470e17981
Frankie Contreras <frankiecontreras@Frankies-iMac.local>
2024-01-17 17:15:55 -0700

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 var searchBtn = $('button');
 var inputEl = $('.form-control');
-var cityName = "";
+var cityName = "London";
 var searchHistory = JSON.parse(localStorage.getItem("search"));
 if(!searchHistory){
   var searchHistory = [];
@@ -95,13 +95,50 @@ function getWeather(cityName) {
         }
         });
 }
+
+// Get UV index function
+function getUV(lon, lat){
+  var UVurl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&appid=7e3a149deb7dcf451641dcd1d05f5cd5";
+  fetch(UVurl)
+    .then(function (response) {
+      return response.json();
+    })
+      .then(function (data) {
+        // Return the result on the DOM   
+        var UVEl = $("<p>");
+        UVEl.html("UV index: ");
+
+         var UV = $("<button>");
+        UV.attr("type", "button").addClass("btn btn-lg").html(data.current.uvi);
+
+        //Attribute the background color depending on the UV index
+        if(data.current.uvi >= 0 && data.current.uvi <3){
+          UV.addClass("bg-success");
+        }else if(data.current.uvi <= 6){
+          UV.addClass("bg-warning");
+        }else if(data.current.uvi <= 6){
+          UV.addClass("bg-orange");
+        }else if(data.current.uvi <= 8){
+          UV.addClass("bg-danger");
+        }else {
+          UV.addClass("bg-violet");
+        }
+
+        // Append the value on the DOM
+        $('.mainResult').append(UVEl);
+        UVEl.append(UV);
+      });
+}
+
+//Generate the last 5 cities searched
+//Make them clickable ?
 function generateHistory(){
-    $('ul').html("");
-    for(i=0; i<searchHistory.length; i++){
-      var historyEl = $("<li>");
-      historyEl.addClass("list-group-item").text(searchHistory[i]);
-      $('ul').append(historyEl)
-    }
-    
+  $('ul').html("");
+  for(i=0; i<searchHistory.length; i++){
+    var historyEl = $("<li>");
+    historyEl.addClass("list-group-item").text(searchHistory[i]);
+    $('ul').append(historyEl)
   }
-  searchBtn.on('click', searchCity);
+  
+}
+searchBtn.on('click', searchCity);
